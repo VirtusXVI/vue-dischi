e1a7611693343bdc5e3611a2b4e8c6df4c331c00
VirtusXVI <manuel.aveta2001@gmail.com>
2022-07-08 14:59:21 +0200

    completed header

---
+++ src/components/MyHeader.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <div class="header-background">
+        <img src="../assets/img/Spotify.png" alt="">
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+    name: "MyHeader"
+}
+</script>
+
+<style lang="scss" scoped>
+    .header-background{
+        background-color: #2e3a46;
+        padding: 0.7rem 1rem;
+    }
+    img{
+        height: 50px;
+    }
+</style>
